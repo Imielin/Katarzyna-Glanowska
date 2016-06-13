e649c05a286585dae12d93ce3c38bd81bd04ecf8
Imielin <maciekbojdak@gmail.com>
2016-06-13 12:31:50 +0200

    change2
    change2

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import statistics
 from sqlalchemy import Column, Integer, String, Text
 import statistics
 from sqlalchemy import create_engine
